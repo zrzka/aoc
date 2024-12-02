d978bc1b0c20173c89e9946cf2d42b4af90904d4
Robert Vojta <robert@pspdfkit.com>
2024-12-02 20:36:42 +0100

    Day 1

---
+++ 2024/day-1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# https://adventofcode.com/2024/day/1
+
+require "bundler/setup"
+Bundler.require
+
+first, second = File
+  .foreach("#{__dir__}/day-1.txt")
+  .map { |x| x.split.map(&:to_i) }
+  .transpose
+
+first.sort!
+second.sort!
+
+result = first.zip(second)
+  .reduce(0) { |acc, (a, b)| acc = acc + (a - b).abs }
+
+puts result
